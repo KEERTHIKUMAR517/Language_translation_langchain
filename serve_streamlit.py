7fa3b967f3a674d072514d53390bbb6ec68d34a5
KEERTHIKUMAR517 <94115016+KEERTHIKUMAR517@users.noreply.github.com>
2024-10-02 10:33:30 +0100

    Update serve_streamlit.py

--- serve_streamlit.py
+++ serve_streamlit.py
@@ -22,7 +22,7 @@ model = ChatGroq(
 )
 
 # Create a chat prompt template
-system_template = "You are a helpful assistant that translates {input_language} to {output_language}. just give me the translated text"
+system_template = "You are a helpful assistant that translates {input_language} to {output_language}. just give me the translated text if non-english give text along with english version of tranlated in parenthesis"
 human_template = "{text}"
 
 chat_prompt = ChatPromptTemplate.from_messages([
